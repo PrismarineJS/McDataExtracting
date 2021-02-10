a0db48d61fe784fb26a49797e8e77c1544931066
u9g <43508353+u9g@users.noreply.github.com>
2021-02-10 17:42:27 -0500

    Added script to wrap getting fabric version

---
+++ start.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch')
+const fs = require('fs/promises')
+const { once } = require('events')
+
+const wantedVersion = '1.16.5'
+
+function getNewest (arr) {
+  return arr.reduce((prev, current) => {
+    return prev.build > current.build ? prev : current
+  }, {})
+}
+
+async function start () {
+  const res = await fetch('https://meta.fabricmc.net/v2/versions/yarn/')
+  const json = await res.json()
+  const filtered = json.filter(x => x.gameVersion === wantedVersion)
+  const version = getNewest(filtered)?.version
+  if (!version) {
+    console.error('There was a problem with the version supplied.')
+    return
+  }
+  const text = `minecraft_version=${wantedVersion}\nyarn_mappings=${version}`
+  await fs.writeFile('gradle.properties', text)
+  console.info('Config file written.')
+  const child = require('child_process').exec('gradle run')
+  // child.stdout.pipe(process.stdout) // if you want the output of gradle
+  await once(child, 'exit')
+  console.info('Gradle done.')
+}
+
+start()
