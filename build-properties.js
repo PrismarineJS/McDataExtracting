ec61035254af8cc5d4f738ad2d15da35ad9da143
u9g <43508353+u9g@users.noreply.github.com>
2021-06-10 16:39:15 -0400

    get fabric loom version automatically (#9)

--- build-properties.js
+++ build-properties.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch')
+const parser = require('fast-xml-parser')
 const fs = require('fs/promises')
 const { once } = require('events')
 
@@ -9,18 +10,28 @@ function getNewest (arr) {
 }
 
 async function start () {
-  const [,,wantedVersion] = process.argv
+  const [,, wantedVersion] = process.argv
+  const yarnMappingsVersion = await getYarnMappingsVersion(wantedVersion)
+  const fabricLoomVersion = await getFabricLoomVersion()
+  const text = `minecraft_version=${wantedVersion}\nyarn_mappings=${yarnMappingsVersion}\nfabric_loom_version=${fabricLoomVersion}`
+  await fs.writeFile('gradle.properties', text)
+  console.info('Config file written.')
+}
+
+async function getYarnMappingsVersion (wantedVersion) {
   const res = await fetch('https://meta.fabricmc.net/v2/versions/yarn/')
   const json = await res.json()
   const filtered = json.filter(x => x.gameVersion === wantedVersion)
-  const version = getNewest(filtered)?.version
-  if (!version) {
+  const yarnMappingsVersion = getNewest(filtered)?.version
+  if (!yarnMappingsVersion) {
     console.error('There was a problem with the version supplied.')
-    return
   }
-  const text = `minecraft_version=${wantedVersion}\nyarn_mappings=${version}`
-  await fs.writeFile('gradle.properties', text)
-  console.info('Config file written.')
+  return yarnMappingsVersion
+}
+
+async function getFabricLoomVersion () {
+  const xmlData = (await (await fetch('https://maven.fabricmc.net/net/fabricmc/fabric-loom/maven-metadata.xml')).text())
+  return parser.parse(xmlData).metadata.versioning.release
 }
 
 start()
